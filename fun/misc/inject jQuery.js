4e6dcca027937eafee1ce0eaeb91a52a1ec86e30
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-02 19:30:23 -0800

    Create inject jQuery.js

---
+++ fun/misc/inject jQuery.js
@@ -0,0 +1,3 @@
+javascript:void(function(){document.body.appendChild(document.createElement('script')).src='http://code.jquery.com/jquery-1.7.2.min.js' })();
+
+//Inject JQuery to test, debug, modify and hack the client side view of a page, so you can run scripts and debug with Firebug or Chrome dev tools.
