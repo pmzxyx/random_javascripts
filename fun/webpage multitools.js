7ca539ca58d2b5b3e34778c7c3799c3030cd5da5
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-02 18:48:59 -0800

    Create webpage multitools.js

---
+++ fun/webpage multitools.js
@@ -0,0 +1,1 @@
+javascript:(function%20()%20%7Bvar%20v%20%3D%20document.createElement(%27script%27)%3Bv.src%20%3D%20%27https%3A%2F%2Fcdn.jsdelivr.net%2Fgh%2FBrowncha023%2FVengeance%40v1.2.0%2Fscript.min.js%27%3Bdocument.body.appendChild(v)%3B%7D())
