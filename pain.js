b69605dc122727bdd27f8e91c633f5c7b0359858
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-03 13:46:45 -0800

    Create pain.js

---
+++ pain.js
@@ -0,0 +1,1 @@
+javascript:function%20a(e)%7Bvar%20n=e.childNodes;for(var%20i%20in%20n)%7Ba(n[i]);if(n[i].style)%20n[i].style.backgroundImage="url(https://i.chzbgr.com/full/5759452672/h934FBF16/my-eyes-my-eyessssssssss)";%7D%7D%20a(document);
