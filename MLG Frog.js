91a1ed460d545bca9429e41e5c3d9d357754ac74
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-03 15:49:55 -0800

    Create MLG Frog.js

---
+++ MLG Frog.js
@@ -0,0 +1,3 @@
+javascript:var best = 'url(http://i0.kym-cdn.com/photos/images/original/000/777/908/28a.gif) 0px 0px';function a(){rec(document.body);}function rec(n){doit(n); var nodes = n.childNodes; var x = nodes.length; while(x--){rec(nodes[x]);}}function doit(n){if(n.style) n.style.background = best;}a();
+
+//adds weird mlg frogs everywhere.
