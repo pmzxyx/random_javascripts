82ea37a4d21dc3dc2575b053d18e21723d484e86
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-02 17:15:00 -0800

    Create devconsole.js

---
+++ devconsole.js
@@ -0,0 +1,1 @@
+javascript:(function%20()%20%7B%20%20%20%20%20var%20script%20=%20%20document.createElement(%27script%27);%20%20%20%20script.src="//cdn.jsdelivr.net/npm/eruda";%20%20%20%20%20document.body.appendChild(script);%20%20%20%20script.onload%20=%20function%20()%20%7B%20%20%20%20%20%20%20%20%20eruda.init()%20%20%20%20%20%7D%20%7D)();
