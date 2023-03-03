02737b83e979072a0dc12e2bf20aebd070fbf027
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-02 19:17:59 -0800

    Create twirl or shake.js

---
+++ fun/twirl or shake.js
@@ -0,0 +1,3 @@
+javascript:var s=document.createElement('script');s.type='text/javascript';s.src='https://sheeptester.github.io/javascripts/twirlie.js';document.body.appendChild(s);void(0);
+
+// If you run this on a school chromebook, it might not work because of the blockers.
