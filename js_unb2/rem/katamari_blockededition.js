e62c1f4b592b0e77a015c578f5a84104f65493a9
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-03 13:07:21 -0800

    Create katamari_blockededition.js

---
+++ js_unb2/rem/katamari_blockededition.js
@@ -0,0 +1,1 @@
+var i,s,ss=['https://githubraw.com/pmzxyx/random_javascripts/main/js_unb2/katamari_scriptloader.js','http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
