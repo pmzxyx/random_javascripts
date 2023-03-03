6e64bf0435cacff8efc6299bc1e9fac8ea941401
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-02 20:13:05 -0800

    Create NYT articles for free.js

---
+++ fun/misc/NYT articles for free.js
@@ -0,0 +1,3 @@
+javascript:(()=>{function rm(el) {el.parentElement.removeChild(el);}rm(document.getElementById('gateway-content'));document.getElementById('site-content').style.position = 'initial';})();
+
+//no more fees
