aef429e157bd6f08cc0b5b969ea84205e347b1f4
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-02 19:24:09 -0800

    Create JS Panel.js

---
+++ fun/JS Panel.js
@@ -0,0 +1,8 @@
+
+// regular style
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://caiorss.github.io/commandpanel.js' })();
+//school style (when school extensions blocked it)
+
+
+
+//has a few javascript functions.
