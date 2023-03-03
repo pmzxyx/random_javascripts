989aade47f893eb927e33a408b23c624639b8a51
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-02 19:28:38 -0800

    Create Bypass Youtube adult filter.js

---
+++ fun/misc/Bypass Youtube adult filter.js
@@ -0,0 +1,4 @@
+// run this on a yt video
+javascript:(function(){window.location.replace(document.URL.replace('watch?v=%27,%27v/%27))})()
+                                                                    
+                                                                    
