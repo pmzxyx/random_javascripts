ac0a9b9f44f032721d8ce3337b4ee6fae83e9002
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-02 17:18:10 -0800

    javascript executor (sheeptester)
    Create evaljs.js

---
+++ evaljs.js
@@ -0,0 +1,1 @@
+javascript:var%20s=document.createElement(%27script%27);s.type=%27text/javascript%27;s.src=%27https://sheeptester.github.io/javascripts/eval.js%27;document.body.appendChild(s);void(0);
