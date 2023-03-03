321bda630d3b801f36a87874cf54ab8646128bb8
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-02 19:26:27 -0800

    Update JS Panel.js

--- fun/JS Panel.js
+++ fun/JS Panel.js
@@ -2,7 +2,8 @@
 // regular style
 javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://caiorss.github.io/commandpanel.js' })();
 //school style (when school extensions blocked it)
+javascript:(function(){document.body.appendChild(document.createElement('script')).src='http://raw.githubusercontent.com/pmzxyx/random_javascripts/main/js_unb2/commandpanel.js' })();
 
 
-
+//https://raw.githubusercontent.com/pmzxyx/random_javascripts/main/js_unb2/commandpanel.js
 //has a few javascript functions.
