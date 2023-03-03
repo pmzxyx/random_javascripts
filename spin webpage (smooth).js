5bf0384862c615b28974448a8327ddaa840575e8
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-03 14:05:23 -0800

    Create spin webpage (smooth).js

---
+++ spin webpage (smooth).js
@@ -0,0 +1,3 @@
+javascript:var $jscomp=$jscomp||{};$jscomp.scope={},$jscomp.createTemplateTagFirstArg=function(e){return e.raw=e},$jscomp.createTemplateTagFirstArgWithRaw=function(e,n){return e.raw=n,e};var css="\nhtml, body { auto;}",style=document.createElement("style");style.innerHTML=css,document.head.appendChild(style),css="\n@keyframes roll {\n  100%{\n    transform:rotate(360deg)\n  }\n}\nbody {\n  animation-name: roll;\n  animation-duration: 4s;\n  animation-iteration-count: 1;\n}\n",(style=document.createElement("style")).innerHTML=css,document.head.appendChild(style);
+
+//i made it so it has smooth animation
