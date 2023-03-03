7ebcec6b1665544ac9cb763c07277c0331a2de4c
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-02 18:17:34 -0800

    Create run javascript.js

---
+++ run javascript.js
@@ -0,0 +1,3 @@
+javascript:(()=>{var askedCommand = prompt("what command");if (askedCommand !== null) {location="javascript: (() => {"+askedCommand+"})();"};})();
+
+// will not work if javascript is disabled, like all the others.
