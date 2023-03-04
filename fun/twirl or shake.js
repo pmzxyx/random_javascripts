84e8f3014ca65ed3702cdd1acdf1a6056004d026
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-04 09:50:41 -0800

    Update twirl or shake.js

--- fun/twirl or shake.js
+++ fun/twirl or shake.js
@@ -1,3 +1,5 @@
+//regular version
 javascript:var s=document.createElement('script');s.type='text/javascript';s.src='https://sheeptester.github.io/javascripts/twirlie.js';document.body.appendChild(s);void(0);
 
-// If you run this on a school chromebook, it might not work because of the blockers.
+//school version:
+var s=document.createElement('script');s.type='text/javascript';s.src='https://cdn.jsdelivr.net/gh/pmzxyx/random_javascripts@main/twirlie.js';document.body.appendChild(s);void(0);
