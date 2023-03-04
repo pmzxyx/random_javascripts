158b3ada13c4618fcbc01f6fe06446f7f76c3ab4
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-03 16:14:25 -0800

    Create make reading easy.js

---
+++ make reading easy.js
@@ -0,0 +1,1 @@
+javascript: var style = document.createElement('style');style.innerText = '\*{line-height: 2em !important;font-size: 18px !important;font-family: Quicksand, Lato, Tahoma, Helvetica, Arial, sans-serif !important;}';var head = document.getElementsByTagName('head')[0];head.appendChild(style);
