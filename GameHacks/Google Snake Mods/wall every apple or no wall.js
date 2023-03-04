8cb5a6d9b4ab7660863ac880d1220c0771d8f8be
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-03 18:11:16 -0800

    Update wall every apple or no wall.js

--- GameHacks/Google Snake Mods/wall every apple or no wall.js
+++ GameHacks/Google Snake Mods/wall every apple or no wall.js
@@ -1,1 +1,6 @@
+//First, insert this script.
 javascript: req = new XMLHttpRequest(); req.open('GET', 'https://raw.githubusercontent.com/DarkSnakeGang/GoogleSnakeWall-EndgameSooner/main/code.js'); req.onload = function() { eval(this.responseText + 'snake.wall_every_apple();'); }; req.send();
+
+//Then, insert this script
+snake.wall_every_apple();
+//into the console.
