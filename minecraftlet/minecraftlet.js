dc0f781995afdec72ddb542b360af7b877cc212d
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-04 10:16:29 -0800

    Create minecraftlet.js

---
+++ minecraftlet/minecraftlet.js
@@ -0,0 +1,2 @@
+(function(){document.body.appendChild(document.createElement('script')).src='https://cdn.jsdelivr.net/gh/pmzxyx/random_javascripts@main/minecraftlet/load.js';})();
+// FIX :random white backgrounds
