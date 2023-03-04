94cb9990e638a151a833d6e9982f0ad2011cf9df
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-04 09:55:41 -0800

    Create katamari.js

---
+++ fun/katamari.js
@@ -0,0 +1,7 @@
+//regular
+
+javascript:var i,s,ss=['http://kathack.com/js/kh.js','http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
+
+//school unblocked version
+
+var i,s,ss=['http://cdn.jsdelivr.net/gh/pmzxyx/random_javascripts@main/js_unb2/katamari_scriptloader.js','http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
