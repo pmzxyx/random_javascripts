184ed7bb686fc9ebc78a21a2f3a5b2b5dacead46
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-04 10:00:05 -0800

    Create fontbomb.js

---
+++ fun/fontbomb.js
@@ -0,0 +1,6 @@
+//regular
+
+javascript:(function () {var s = document.createElement('script');s.setAttribute('src', 'http://fontbomb.ilex.ca/js/main.js');document.body.appendChild(s);}());
+
+//school unblocked version
+(function () {var s = document.createElement('script');s.setAttribute('src', 'http://cdn.jsdelivr.net/gh/pmzxyx/random_javascripts@main/js_unb2/fontbomb/main.js');document.body.appendChild(s);}());
