6d2eaada75f942a905eb5697628376918eebe5e3
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-04 10:57:27 -0800

    Create simple darkmode.js

---
+++ simple darkmode.js
@@ -0,0 +1,2 @@
+javascript:all=document.getElementsByTagName("*");for(let i=0;i<all.length;i++)void 0!==all[i].style&&(all[i].style.fontFamily="Comic Sans MS");document.getElementsByTagName("html")[0].style.filter="invert()",elems=document.querySelectorAll(" a, img, video");for(let j=0;j<elems.length;j++)("A"==elems[j].nodeName&&(""!=elems[j].style.background||""!=elems[j].style.backgroundImage)||"A"!=elems[j].nodeName)&&(elems[j].style.filter="invert()");
+
