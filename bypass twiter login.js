3c194ffebd1eb9e3b539822f2bd6692c0ed4c9c5
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-04 11:07:51 -0800

    Create bypass twiter login.js

---
+++ bypass twiter login.js
@@ -0,0 +1,1 @@
+javascript:document.querySelectorAll('[role="dialog"]')[document.querySelectorAll('[role="dialog"]').length-1].remove();document.getElementsByTagName("html")[0].style.overflowY="scroll";
