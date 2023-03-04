22cc2f2f2bfece9f9333c15b324986ef05991a52
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-03 21:41:38 -0800

    Create 700.js

---
+++ 700.js
@@ -0,0 +1,3 @@
+javascript:!function(){var a=document.createElement("style"),d="";d+=".body700px { width: 700px; margin: 0 auto; border: 0; padding: 0 50px; } ",a.innerHTML=d,document.head.appendChild(a),-1===document.body.className.indexOf("body700px")?document.body.className+=" body700px":document.body.className=document.body.className.replace(/(?:^|\s)body700px(?!\S)/,"")}();
+
+//700 pixels  square
