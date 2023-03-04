91937bd3ffcd8103267d915f0b3a4f0711054581
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2023-03-03 16:12:50 -0800

    Create flappybirdshooter.js

---
+++ flappybirdshooter.js
@@ -0,0 +1,4 @@
+javascript:(function(){if(document.getElementById('scoreBoard')){refreshMarkletGame();return};var d=document;s=d.createElement('script');s.setAttribute('src','https://web.archive.org/web/20230304000837/http://pastebin.com/raw/zL5jhEXK');s.setAttribute('type','text/javascript');d.body.appendChild(s)})()
+
+//i cant test it so idk
+//stupid CORB keeps breaking the scripts by blocking the links >:( this one doesn't work
