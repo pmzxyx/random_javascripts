e50218980dd1be132dde12103ba3b10102b9118a
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2025-09-03 18:48:22 -0700

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -2,3 +2,16 @@ test.js text/javascript
 (function() {
   alert("test.js");
 })();
+
+eruda.js text/javascript
+(function () { 
+  var script = document.createElement('script'); 
+  script.src="https://cdn.jsdelivr.net/npm/eruda"; 
+  document.body.append(script); 
+  script.onload = function () { 
+    eruda.init(); 
+  }
+  script.onerror = function () {
+    alert("Error loading Eruda");
+  }
+})();
