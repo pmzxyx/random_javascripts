594ff7d77182c1d1902ab918854b1b8429060ec6
pmzxyx <126836542+pmzxyx@users.noreply.github.com>
2025-09-03 19:10:56 -0700

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -1,15 +1,23 @@
 eruda.js text/javascript
 (function () { 
-  var script = document.createElement('script'); 
-  script.src="https://cdn.jsdelivr.net/npm/eruda"; 
-  document.body.append(script); 
-  script.onload = function () { 
-    eruda.init(); 
-    console.log("Eruda loaded");
-  };
-  script.onerror = function () {
-    alert("Error loading Eruda");
-    console.log("Eruda not loaded");
+  try {
+  window.onload = function() {
+    console.log("Launch_Eruda");
+    var script = document.createElement('script'); 
+    script.src="https://cdn.jsdelivr.net/npm/eruda"; 
+    document.body.append(script); 
+    script.onload = function () { 
+      eruda.init(); 
+      console.log("Eruda loaded");
+    };
+    script.onerror = function () {
+      alert("Error loading Eruda");
+      console.log("Eruda not loaded");
+    };
+   };
+  } catch (error) {
+    alert(error);
+    console.log(error);
   };
 })();
 
